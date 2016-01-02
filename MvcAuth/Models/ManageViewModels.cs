989f351702583780e0fa1bf9222a006a3095d5a2
Vladimir <ikondratenkova@gmail.com>
2016-01-03 00:10:33 +0200

    Наудалял ещё, в хэлпере

--- MvcAuth/Models/ManageViewModels.cs
+++ MvcAuth/Models/ManageViewModels.cs
@@ -9,8 +9,6 @@ namespace MvcAuth.Models
     {
         public bool HasPassword { get; set; }
         public IList<UserLoginInfo> Logins { get; set; }
-        public string PhoneNumber { get; set; }
-        public bool TwoFactor { get; set; }
         public bool BrowserRemembered { get; set; }
 
         public string FullName { get; set; }
